81cf868230f76a037fe26db0e5e42864779f0202
BRYAN MD <bryan@MacBook-Pro-de-BRYAN.local>
2024-01-16 09:50:23 +0100

    fix(server): add files

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,10 +8,6 @@ app.get('/', (_req, res)=>{
     res.send('hola mundo')
 });
 
-app.get('/prueba', (_req, res)=>{
-    res.send('adios mundo')
-});
-
 
 app.listen(PORT, ()=>{
     console.log(`app corriendo en http://localhost:${PORT}`)
