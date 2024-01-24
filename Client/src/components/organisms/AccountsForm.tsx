d2719f3fa46a4d37520d27ec84f36f5392f8b3ab
VirginiaSantana <virsantana@gmail.com>
2024-01-24 14:38:20 +0100

    feat(feature/recover-password): createAccountsForm

--- Client/src/components/organisms/AccountsForm.tsx
+++ Client/src/components/organisms/AccountsForm.tsx
@@ -4,11 +4,11 @@ const AccountsForm =() => {
     return (
         <>
         
-            <section className="form-password w-screen h-screen flex justify-center items-center flex-col gap-4 bg-white">
-            <div className="flex justify-between items-center space-x-10 mb-6">
-                <img src="/src/images/account-icon.png" alt="account-icon" className="h-10 align-middle"/>
+            <section className="form-password w-screen h-screen flex justify-center items-center flex-col gap-16 bg-white">
+            <div className="flex gap-8 justify-center items-center">
+                <img src="/src/images/account-icon.png" alt="account-icon" className="h-10"/>
                 <h1 className="
-            text-primary font-medium text-5xl mb-6 flex-1">Cuentas</h1>
+            text-primary font-medium text-5xl flex-1">Cuentas</h1>
             </div>    
 
             <form action="" className="flex flex-col w-2/4 gap-3 mt-6">
@@ -20,17 +20,61 @@ const AccountsForm =() => {
                 </div>
             </form>
 
-            <div className="grid grid-cols-8 grid-rows-8 gap-4">
-                <div className="col-start-1 row-start-1 bg-gray-200 p-4 text-black text-sm">Celda 1</div>
-                {Array.from({ length: 64 }, (_, i) => (
-                <div key={i}
-                className={`col-start-${(i % 8) + 1} row-start-${Math.floor(i / 8) + 1} bg-gray-200 p-4 text-center`}>
-                {i + 1}
+        <section className="flex flex-col w-full items-center gap-4">
+
+            <div className="flex gap-4 border bg-white w-4/5">
+                <div className="w-16 h-full">
+
                 </div>
-            ))}
+        
+                <div className="p-4 text-black text-sm font-bold w-40">
+                    Aplicaciones
+                </div>
+                <div className="p-4 text-black text-sm font-bold w-40">
+                    Correo Electrónico
+                </div>
+                <div className="p-4 text-black text-sm font-bold w-40">
+                    Categoría
+                </div>
+                <div className="p-4 text-black text-sm font-bold w-40">
+                Contraseña
+                </div>
+                <div className="p-4 text-black text-sm font-bold w-40">
+                Ver
+                </div>
+                <div className="p-4 text-black text-sm font-bold w-40">
+                Editar / Eliminar
+                </div>
             </div>
 
-            <div className="flex items-center justify-start gap-4 text-left">
+            <div className="flex gap-4 border bg-white w-4/5">
+                <div className="p-4 text-black text-sm font-bold w-16">
+                    <img src="/src/images/linkedin-icon.png" alt="LinkedIn icon" className="h-8" />
+                </div>
+                <div className="p-4 text-black text-sm font-bold w-40">
+                    LinkedIn
+                </div>
+                <div className="p-4 text-black text-sm font-bold w-40">
+                    prueba@gmail.com
+                </div>
+                <div className="p-4 text-black text-sm font-bold w-40">
+                    Redes Sociales
+                </div>
+                <div className="p-4 text-black text-sm font-bold w-40">
+                LJu34gT@#
+                </div>
+                <div className="p-4 text-black text-sm font-bold w-40">
+                <img src="/src/images/open-eye-icon.png" alt="open-eye-icon" className="h-5" />
+                </div>
+                <div className="flex p-4 text-black text-sm font-bold w-40 gap-4">
+                    <img src="/src/images/pen-icon.png" alt="pen-icon-icon" className="h-5 "/>
+                    <img src="/src/images/trash-icon.png" alt="trash-icon" className="h-5"/>
+                </div>
+            </div>
+
+        </section>
+
+            <div className="flex items-center w-2/4 justify-start gap-4">
                 <img src="/src/images/plus-icon.png" alt="plus-icon" />
                 <small className="
             text-primary font-small text-xl">Añadir nueva aplicación</small>
--- Client/src/components/organisms/RecoverPasswordForm.tsx
+++ Client/src/components/organisms/RecoverPasswordForm.tsx
@@ -4,11 +4,11 @@ const RecoverPasswordForm =() => {
     return (
         <>
         
-        <section className="form-password w-screen h-screen flex justify-center items-center flex-col gap-4 bg-white">
-            <div className="flex justify-between items-center space-x-10 mb-6">
-                <img src="/src/images/padlock-icon.png" alt="padlock-icon" className="h-10 align-middle"/>
+        <section className="form-password w-screen h-screen flex justify-center items-center flex-col gap-16 bg-white">
+            <div className="flex gap-8 justify-center items-center">
+                <img src="/src/images/padlock-icon.png" alt="padlock-icon" className="h-10"/>
                 <h1 className="
-            text-primary font-medium text-5xl mb-6 flex-1">Recuperar Contraseña Maestra</h1>
+            text-primary font-medium text-5xl flex-1">Recuperar Contraseña Maestra</h1>
             </div>
 
         <form action="" className="flex flex-col w-2/4 gap-3">
@@ -22,7 +22,8 @@ const RecoverPasswordForm =() => {
             <input className="bg-primary border border-black rounded p-3" type="submit" value="Confirmar" />
         </form>
 
-        <div className="flex justify-between items-center space-y-10">
+        <div className="flex flex-col gap-16 items-center">
+        <div className="flex gap-4 justify-center items-center w-full">
             <h1 className="text-primary space-y-10">#/lisay-maggi35</h1>
             <img src="/src/images/file-upload-icon.png" alt="file-upload-icon.png" />
         </div>
@@ -30,7 +31,7 @@ const RecoverPasswordForm =() => {
             <small className="text-black font-bold text-lg">¿Quieres establecer una nueva contraseña maestra?</small>
 
             <small className="text-primary font-bold text-lg">Establecer nueva contraseña maestra</small>
-
+            </div>
         </section>
     </>
     )
