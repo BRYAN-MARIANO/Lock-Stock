b0636003d877054000f6ff879fb46bc81353055f
angu-m9 <n.angulopop@gmail.com>
2024-02-02 09:23:06 +0100

    fix(authentication_review):refactor login controller and change Name_User for Email_Userto match object received from login component

--- Client/src/services/services.ts
+++ Client/src/services/services.ts
@@ -251,7 +251,7 @@ class Services {
 
   async register(credentials: FieldValues) {
     try {
-      const response = await fetch("http://localhost:3000/users", {
+      const response = await fetch("http://localhost:4000/users", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(credentials),
--- server/src/controllers/loginController.ts
+++ server/src/controllers/loginController.ts
@@ -8,74 +8,152 @@ import { ILoginAttempt } from "../../loginAttempsInterface"; // Asumiendo que ti
 import { v4 as generateUuid } from 'uuid';
 import { UserInterface } from "../../userInterface"; // Asume que tienes una interfaz de usuario definida
 
+// export const login = async (req: Request, res: Response) => {
+//   const { Email_User, Password_User } = req.body;
+//   const error400 = () => res.status(400).json({ message: "Credenciales Inválidas" });
+
+//   if (!Email_User || !Password_User) {
+//     return error400();
+//   }
+
+//   let user = await User.findOne({
+//     where: { Email_User: { [Op.eq]: Email_User } },
+//   }) as UserInterface | null;
+
+//   if (!user) {
+//     console.log("Usuario no encontrado");
+//     await createLoginAttempt(null,  req); // Falso indica que el intento es fallido
+//     return error400();
+//   }
+
+//   // Afirmamos que el tipo de 'user' es 'UserInterface' después de verificar que no es nulo
+//   const userTyped = user!.get() as UserInterface;
+
+//   const isPasswordValid = await bcrypt.compare(Password_User, userTyped.Password_User);
+//   console.log("Contraseña válida:", isPasswordValid);
+//   if (!isPasswordValid) {
+//     const maxAttempts = 5;
+//     await user.increment('loginAttempts') as UserInterface;
+//     await user.reload()  as UserInterface;
+//     console.log("Intentos de login después de incrementar:", userTyped.loginAttempts);
+
+
+//     await createLoginAttempt(userTyped.Id_User,  req);
+
+//     if (userTyped.loginAttempts >= maxAttempts) {
+//       // Lógica para bloquear al usuario y enviar email
+//       // ...
+//     }
+
+//     return error400();
+//   }
+
+//   if (isPasswordValid) {
+//     // Resetea los intentos fallidos si el inicio de sesión es exitoso
+//     await user.update({ loginAttempts: 0 })  as UserInterface ;
+
+//     // Genera y guarda el token de sesión
+//     const secretKey = process.env.SECRET_KEY!;
+//     const token = jwt.sign({
+//       Email_User: user.Email_User,
+//       location: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
+//       device: req.headers["user-agent"],
+//       connectionTime: new Date(),
+//     }, secretKey, { expiresIn: "1h" });
+
+//     const hashedToken = await bcrypt.hash(token, 10);
+//     const location = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+//     const locationString = Array.isArray(location) ? location.join(", ") : location;
+//     await user.update({
+//       hashedToken,
+//       location: locationString,
+//       device: req.headers["user-agent"],
+//       connectionTime: new Date(),
+//     });
+
+//     return res.json({ message: "Logged in successfully", accessToken:token });
+//   }
+
+//   return error400();
+// };
+
 export const login = async (req: Request, res: Response) => {
-  const { Name_User, Password_User } = req.body;
+  const { Email_User, Password_User } = req.body;
   const error400 = () => res.status(400).json({ message: "Credenciales Inválidas" });
 
-  if (!Name_User || !Password_User) {
+  if (!Email_User || !Password_User) {
     return error400();
   }
 
-  let user = await User.findOne({
-    where: { Name_User: { [Op.eq]: Name_User } },
-  }) as UserInterface | null;
-
+  let user = await findUserByEmail(Email_User);
   if (!user) {
     console.log("Usuario no encontrado");
-    await createLoginAttempt(null,  req); // Falso indica que el intento es fallido
+    await createLoginAttempt(null, req); // Intento fallido
     return error400();
   }
 
-  // Afirmamos que el tipo de 'user' es 'UserInterface' después de verificar que no es nulo
-  const userTyped = user!.get() as UserInterface;
-
-  const isPasswordValid = await bcrypt.compare(Password_User, userTyped.Password_User);
-  console.log("Contraseña válida:", isPasswordValid);
-  if (!isPasswordValid) {
-    const maxAttempts = 5;
-    await user.increment('loginAttempts') as UserInterface;
-    await user.reload()  as UserInterface;
-    console.log("Intentos de login después de incrementar:", userTyped.loginAttempts);
-
-
-    await createLoginAttempt(userTyped.Id_User,  req);
-
-    if (userTyped.loginAttempts >= maxAttempts) {
-      // Lógica para bloquear al usuario y enviar email
-      // ...
-    }
-
+  if (!(await isPasswordValid(user, Password_User))) {
+    await handleFailedLoginAttempt(user, req);
     return error400();
   }
 
-  if (isPasswordValid) {
-    // Resetea los intentos fallidos si el inicio de sesión es exitoso
-    await user.update({ loginAttempts: 0 })  as UserInterface ;
+  await resetLoginAttempts(user);
+  const token = await generateUserToken(user, req);
+  await updateUserTokenInfo(user, token, req);
 
-    // Genera y guarda el token de sesión
-    const secretKey = process.env.SECRET_KEY!;
-    const token = jwt.sign({
-      Name_User: user.Name_User,
-      location: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
-      device: req.headers["user-agent"],
-      connectionTime: new Date(),
-    }, secretKey, { expiresIn: "1h" });
-
-    const hashedToken = await bcrypt.hash(token, 10);
-    const location = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
-    const locationString = Array.isArray(location) ? location.join(", ") : location;
-    await user.update({
-      hashedToken,
-      location: locationString,
-      device: req.headers["user-agent"],
-      connectionTime: new Date(),
-    });
-
-    return res.json({ message: "Logged in successfully", token });
-  }
-
-  return error400();
+  return res.json({ message: "Logged in successfully", accessToken: token });
 };
+
+async function findUserByEmail(email: string): Promise<UserInterface | null> {
+  return User.findOne({
+    where: { Email_User: { [Op.eq]: email } },
+  }) as Promise<UserInterface | null>;
+}
+
+async function isPasswordValid(user: UserInterface, password: string): Promise<boolean> {
+  return bcrypt.compare(password, user.Password_User);
+}
+
+async function handleFailedLoginAttempt(user: UserInterface, req: Request): Promise<void> {
+  const maxAttempts = 5;
+  await user.increment('loginAttempts');
+  await user.reload();
+  console.log("Intentos de login después de incrementar:", user.loginAttempts);
+
+  await createLoginAttempt(user.Id_User, req);
+
+  if (user.loginAttempts >= maxAttempts) {
+    // Lógica para bloquear al usuario y enviar email
+    // ...
+  }
+}
+
+async function resetLoginAttempts(user: UserInterface): Promise<void> {
+  await user.update({ loginAttempts: 0 });
+}
+
+async function generateUserToken(user: UserInterface, req: Request): Promise<string> {
+  const secretKey = process.env.SECRET_KEY!;
+  return jwt.sign({
+    Email_User: user.Email_User,
+    location: req.headers["x-forwarded-for"] || req.socket.remoteAddress,
+    device: req.headers["user-agent"],
+    connectionTime: new Date(),
+  }, secretKey, { expiresIn: "1h" });
+}
+
+async function updateUserTokenInfo(user: UserInterface, token: string, req: Request): Promise<void> {
+  const hashedToken = await bcrypt.hash(token, 10);
+  const location = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  const locationString = Array.isArray(location) ? location.join(", ") : location;
+  await user.update({
+    hashedToken,
+    location: locationString,
+    device: req.headers["user-agent"],
+    connectionTime: new Date(),
+  });
+}
+
 
 async function createLoginAttempt(userId: string | null, req: Request): Promise<void> {
   console.log("Creando intento de login para usuario:", userId);
